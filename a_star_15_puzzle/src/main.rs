a29da300feaa2d8b99b1dc2c2bf166536adfeee7
Spiryd <neumann.maks@gmail.com>
2023-03-30 14:36:14 +0200

    8 puzzle done

--- a_star_15_puzzle/src/main.rs
+++ a_star_15_puzzle/src/main.rs
@@ -1,3 +1,17 @@
+mod games;
+use games::puzzle8::*;
+
 fn main() {
-    println!("Hello, world!");
+    let start_state: [[u8; 3]; 3] = random_state();
+    if let Some(path) = a_star_search(start_state) {
+        println!("path length = {}", path.len());
+    } else {
+        println!("Goal state not found.");
+    }
+    let start_state: [[u8; 3]; 3] = [[1, 2, 3], [4, 0, 5], [7, 8, 6]];
+    if let Some(path) = a_star_search(start_state) {
+        println!("path length = {}", path.len());
+    } else {
+        println!("Goal state not found.");
+    }
 }
